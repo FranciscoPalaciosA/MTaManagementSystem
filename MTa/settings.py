e5083a2fe715a2ca66ee61c11b2a61e282191336
FranciscoPalaciosA <f.palacios.arriaga@gmail.com>
2018-10-01 10:36:28 -0500

    Models and migrate

--- MTa/settings.py
+++ MTa/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'profiles.apps.ProfilesConfig',
     'directory.apps.DirectoryConfig',
+    'administrative.apps.AdministrativeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- administrative/models.py
+++ administrative/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ProductionReport(models.Model):
+    self_seed = models.IntegerField(default=0)
+    self_leaf = models.IntegerField(default=0)
+    self_flour = models.IntegerField(default=0)
+    days_per_montt = models.IntegerField(default=0)
+    exch_seed = models.IntegerField(default=0)
+    exch_leaf = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    deleted_at = models.DateTimeField(blank=True, null=True)
