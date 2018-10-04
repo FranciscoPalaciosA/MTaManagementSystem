f9f99cbd889c26efb6446548d338ce96889f0cb3
FranciscoPalaciosA <f.palacios.arriaga@gmail.com>
2018-10-03 20:02:38 -0500

    UI de form lista, lógica casi terminada
    - guardar valor default si campo está vacío

--- administrative/forms.py
+++ administrative/forms.py
@@ -3,11 +3,27 @@ from .models import *
 
 class BeneficiaryForm(forms.ModelForm):
     class Meta:
-    model = Beneficiary
-    fields = ['name','last_name_paternal','last_name_maternal','state','municipality','community_name','num_of_family_beneficiaries','contact_name','contact_phone','account_number','bank_name']  
-    
+        model = Beneficiary
+        fields = [  'name',
+                    'last_name_paternal',
+                    'last_name_maternal',
+                    'state','municipality',
+                    'community_name',
+                    'num_of_family_beneficiaries',
+                    'contact_name',
+                    'contact_phone',
+                    'account_number',
+                    'bank_name']
+
 class ProductionReportForm(forms.ModelForm):
+    exch_seed = forms.IntegerField(required=False)
+    exch_leaf = forms.IntegerField(required=False)
+    
     class Meta:
         model = ProductionReport
-        fields = ['self_seed']
-
+        fields = [  'self_seed',
+                    'self_leaf',
+                    'self_flour',
+                    'days_per_month',
+                    'exch_seed',
+                    'exch_leaf']
--- administrative/urls.py
+++ administrative/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'administrative'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('production_report/', views.fill_Production_Report, name='production_report'),
+    path('production_report/', views.production_report, name='production_report'),
+    path('new_production_report/', views.add_production_report, name='new_production_report'),
 ]
