ec858052c3032243ab3b3cd0797fed01fd4d787c
Alex1497 <a01207765@itesm.mx>
2018-10-07 18:01:34 -0500

    Acabar tests

--- administrative/admin.py
+++ administrative/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import ProductionReport
+from .models import Beneficiary
 
 # Register your models here.
 admin.site.register(ProductionReport)
+admin.site.register(Beneficiary)
--- administrative/tests.py
+++ administrative/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from administrative.models import *
 
 # Create your tests here.
+
+def create_user():
+    user = User.objects.create_user('test', 'test@testuser.com', 'testpassword')
+    base_user = BaseUser.objects.create(user=user, name="name",
+                                        last_name_paternal="last_name_paternal",
+                                        last_name_maternal="last_name_maternal",
+                                        phone_number="phone_number",
+                                        email="email@email.com",
+                                        address="address")
+    base_user.save()
+    return base_user
+
+
+class BeneficiariestTest(TestCase):
+    def test_new_beneficary_only_selfconsumption(self):
+        """
+        Creating a new beneficiary with correct information. Expecting a redirect to /administrative/beneficiaries
+
+        """
+        user = create_user()
+        self.client.login(username="test", password="testpassword")
+        response = self.client.post('/administrative/new_beneficiary', {'name': 'beneTest',
+                                                                                'last_name_paternal': 'Test',
+                                                                                'last_name_maternal': 'Test',
+                                                                                'state': 'testState',
+                                                                                'municipality': 'muniTest',
+                                                                                'community_name' : 'testComm',
+                                                                                'num_of_family_beneficiaries': 5,
+                                                                                'contact_name': 'contactTest',
+                                                                                'contact_phone': '111111',
+                                                                                'account_number': '222222',
+                                                                                'bank_name': 'banco'
+                                                                             })
+        #print("\n\n\n\n"+response)
+        self.assertRedirects(response, '/administrative/beneficiaries', status_code=301, target_status_code=200, msg_prefix='', fetch_redirect_response=True)
