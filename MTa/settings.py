528b7944e57c6bdc7fb63a2a77ac0e840f32b48d
BernardoLaing <A01206492@itesm.mx>
2018-11-22 18:34:21 -0600

    Changed staticfiles_dirs to use path.join

--- MTa/settings.py
+++ MTa/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "static"),
-    '/gallery/images/'
+    os.path.join(BASE_DIR,'/gallery/images/')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
